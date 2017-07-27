2999f7ac811438751e85443fc0bf5f239c93bf9a
Chad Fegley <chadfegley7@gmail.com>
2017-07-27 16:23:48 -0700

    First initial commit

---
+++ Haterade/Supporting Files/Haterade-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  Haterade-Bridging-Header.h
+//  Haterade
+//
+//  Created by Chad Fegley on 7/27/17.
+//  Copyright © 2017 Chad Fegley. All rights reserved.
+//
+
+#ifndef Haterade_Bridging_Header_h
+#define Haterade_Bridging_Header_h
+
+
+#endif /* Haterade_Bridging_Header_h */
